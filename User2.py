5598274a765ba93e5aba355df14d22e3dc794449
Eliza Wu <wueliza21@gmail.com>
2023-03-30 14:03:49 -0400

    Revert "Revert "adding formula""
    This reverts commit b67f00a1ed2b86d048231808105105af6f1a93e1.

--- User2.py
+++ User2.py
@@ -1,4 +1,6 @@
 # user without NN
+import math
+
 import tensorflow.compat.v1 as tf
 import gym
 import numpy as np
@@ -21,49 +23,67 @@ class User(object):  # contain a local actor, critic, global critic
         self.work = [0, 0]  # [0] = task1 [1] = task2
         self.work_type = np.random.choice(2)
 
-    def step(self, edge_price):
+
+    def need_CRB(self, edge_price):
         task_arrival_rate = self.task_arrival_rate
         new_task = np.random.poisson(task_arrival_rate)
+        work_type = self.work_type
 
-        # user does it self
-        overflow = new_task * (self.work_type + 1) - self.CRB
-        overflow = overflow if overflow > 0 else 0
-        do_self_utility = overflow * COST_TO_CLOUD
+        buy = task_arrival_rate*( 1/self.CRB + 1/(self.CRB - math.sqrt(edge_price)))
+        trans = min(new_task*(work_type+1) - self.CRB, buy)
+        trans = trans / (work_type+1)
 
-        # transfer all work to edge
-        trans_utility = new_task * (self.work_type + 1) * edge_price
+        return {work_type:trans}
 
-        # allocate work
-        model1 = pulp.LpProblem("value max", sense=LpMinimize)
-        t0 = pulp.LpVariable('t0', lowBound=0, cat=LpInteger)
-        t1 = pulp.LpVariable('t1', lowBound=0, cat=LpInteger)
-        tcloud = pulp.LpVariable('tcloud', lowBound=0, cat=LpInteger)
 
-        model1 += ((t0 * (self.work_type + 1)) - self.CRB)*COST_TO_CLOUD + t1 * (self.work_type + 1) * edge_price + tcloud * (self.work_type + 1) * COST_TO_CLOUD
-        model1 += (t0 * (self.work_type + 1) - self.CRB)*COST_TO_CLOUD + t1 * (self.work_type + 1) * edge_price + tcloud * (self.work_type + 1) * COST_TO_CLOUD >= 0
-        model1 += t0 + t1 + tcloud == new_task * (self.work_type + 1)
-        model1.solve(PULP_CBC_CMD(msg=0))
-        allo_utility = pulp.value(model1.objective)
 
-        # print(model1)
-        # for v in model1.variables():
-        #     print(f'{v.name} = {v.value()}')
-        # print(allo_utility)
+    def step(self, trans_task, edge_price):
 
-        trans_work = 0
-        work = 0
-        utility = min(do_self_utility, trans_utility, allo_utility)
-        if utility == do_self_utility:
-            trans_work = 0
-            overflow = new_task * (self.work_type + 1) - self.CRB
-            overflow = overflow if overflow > 0 else 0
-            work = new_task
-        elif utility == trans_utility:
-            trans_work = new_task
-            overflow, work = 0, 0
-        elif utility == allo_utility:
-            trans_work = value(t1)
-            work = new_task - trans_work / (self.work_type + 1)
-            overflow = work * (self.work_type + 1) - self.CRB
 
-        return utility, overflow, new_task, {self.work_type: trans_work}, work
+
+    # def step(self, edge_price):
+    #     task_arrival_rate = self.task_arrival_rate
+    #     new_task = np.random.poisson(task_arrival_rate)
+    #
+    #     # user does it self
+    #     overflow = new_task * (self.work_type + 1) - self.CRB
+    #     overflow = overflow if overflow > 0 else 0
+    #     do_self_utility = overflow * COST_TO_CLOUD
+    #
+    #     # transfer all work to edge
+    #     trans_utility = new_task * (self.work_type + 1) * edge_price
+    #
+    #     # allocate work
+    #     model1 = pulp.LpProblem("value max", sense=LpMinimize)
+    #     t0 = pulp.LpVariable('t0', lowBound=0, cat=LpInteger)
+    #     t1 = pulp.LpVariable('t1', lowBound=0, cat=LpInteger)
+    #     tcloud = pulp.LpVariable('tcloud', lowBound=0, cat=LpInteger)
+    #
+    #     model1 += ((t0 * (self.work_type + 1)) - self.CRB)*COST_TO_CLOUD + t1 * (self.work_type + 1) * edge_price + tcloud * (self.work_type + 1) * COST_TO_CLOUD
+    #     model1 += (t0 * (self.work_type + 1) - self.CRB)*COST_TO_CLOUD + t1 * (self.work_type + 1) * edge_price + tcloud * (self.work_type + 1) * COST_TO_CLOUD >= 0
+    #     model1 += t0 + t1 + tcloud == new_task * (self.work_type + 1)
+    #     model1.solve(PULP_CBC_CMD(msg=0))
+    #     allo_utility = pulp.value(model1.objective)
+    #
+    #     # print(model1)
+    #     # for v in model1.variables():
+    #     #     print(f'{v.name} = {v.value()}')
+    #     # print(allo_utility)
+    #
+    #     trans_work = 0
+    #     work = 0
+    #     utility = min(do_self_utility, trans_utility, allo_utility)
+    #     if utility == do_self_utility:
+    #         trans_work = 0
+    #         overflow = new_task * (self.work_type + 1) - self.CRB
+    #         overflow = overflow if overflow > 0 else 0
+    #         work = new_task
+    #     elif utility == trans_utility:
+    #         trans_work = new_task
+    #         overflow, work = 0, 0
+    #     elif utility == allo_utility:
+    #         trans_work = value(t1)
+    #         work = new_task - trans_work / (self.work_type + 1)
+    #         overflow = work * (self.work_type + 1) - self.CRB
+    #
+    #     return utility, overflow, new_task, {self.work_type: trans_work}, work
